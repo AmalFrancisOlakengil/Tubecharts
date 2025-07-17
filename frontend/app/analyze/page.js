e2f8219ab72e6ccbd45515282526acf4e7cdf5c2
AmalFrancisOlakengil <francisamal030@gmail.com>
2025-07-17 22:49:57 +0530

    Update page.js

--- frontend/app/analyze/page.js
+++ frontend/app/analyze/page.js
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { PieChart, Pie, Cell, Tooltip, BarChart, Bar, XAxis, YAxis, Legend, ResponsiveContainer } from 'recharts';
